f7f9726397fb89e9a990d2ee173d815c3f0e67a5
theskirata <patrick.riebe1234@gmail.com>
2022-06-24 20:56:40 +0100

    feat(config.lua): disable ESX by default, change hud and map settings

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
 -- Are you using ESX? Turn this to true if you would like fuel & jerry cans to cost something.
-Config.UseESX = true
+Config.UseESX = false
 
 -- What should the price of jerry cans be?
 Config.JerryCanCost = 100
@@ -14,11 +14,11 @@ Config.FuelDecor = "_FUEL_LEVEL"
 Config.DisableKeys = {0, 22, 23, 24, 29, 30, 31, 37, 44, 56, 82, 140, 166, 167, 168, 170, 288, 289, 311, 323}
 
 -- Want to use the HUD? Turn this to true.
-Config.EnableHUD = true
+Config.EnableHUD = false
 
 -- Configure blips here. Turn both to false to disable blips all together.
-Config.ShowNearestGasStationOnly = true
-Config.ShowAllGasStations = false
+Config.ShowNearestGasStationOnly = false
+Config.ShowAllGasStations = true
 
 -- Modify the fuel-cost here, using a multiplier value. Setting the value to 2.0 would cause a doubled increase.
 Config.CostMultiplier = 1.0
